bbb4399ed0d7701c7872a02d567605ca5d75ff06
Leo Basevich <lbasevich86@gmail.com>
2024-10-19 01:01:50 +0000

    Complete

--- src/main/java/ReturnWordCount.java
+++ src/main/java/ReturnWordCount.java
@@ -5,6 +5,17 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
-        return 0;
+        int result = 0;
+        if (in.length()>0)
+            result+=1;
+
+        char test = ' ';    
+        for (int i = 0; i < in.length()-1; i++){
+            // if (in.substring(i,i+1) == " ")
+            if (in.charAt(i)==test)
+                result+=1;
+        }    
+
+        return result;
     }
 }
